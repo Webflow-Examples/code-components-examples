9e36079fab52b813db99d8a8baf7e2a88f1f8a3c
victoriaplummer <victoria.plummer@webflow.com>
2025-09-14 11:59:23 -0400

    add store locator

--- time-machine-app/packages/astro-backend/astro.config.mjs
+++ time-machine-app/packages/astro-backend/astro.config.mjs
@@ -1,13 +1,31 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import cloudflare from "@astrojs/cloudflare";
+import cloudflare from "@astrojs/cloudflare"; // Import the Cloudflare adapter
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  base: "/map",
+  build: {
+    assets: "static",
+  },
   adapter: cloudflare({
     platformProxy: {
       persist: true,
     },
   }),
+  integrations: [react()],
+  // Optimize the build configuration for the Edge runtime
+  vite: {
+    resolve: {
+      // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
+      // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
+      alias: import.meta.env.PROD
+        ? {
+            "react-dom/server": "react-dom/server.edge",
+          }
+        : undefined,
+    },
+  },
 });
