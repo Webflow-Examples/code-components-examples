27e2a157faa746f68b9cf8211e389f09adf12a02
Zach Plata <zach.plata@webflow.com>
2025-10-24 10:11:40 -0500

    fix input interactivity

--- shadcn-components/webpack.webflow.js
+++ shadcn-components/webpack.webflow.js
@@ -1,11 +1,10 @@
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const path = require("path");
+// // eslint-disable-next-line @typescript-eslint/no-require-imports
+// const path = require("path");
 
 module.exports = {
-  mode: "development",
   resolve: {
     alias: {
-      "@": path.resolve(process.cwd()), // Maps @ to the v4 app directory
+      "@": process.cwd(), // Maps @ to the v4 app directory
     },
   },
 };
